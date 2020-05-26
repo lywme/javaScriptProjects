3800d092bf79cde676c0a33b1a362d454ddc8222
Yewei Li <38413224+lywme@users.noreply.github.com>
2020-05-25 22:45:39 -0400

    optimized js

--- project_4/js/main.js
+++ project_4/js/main.js
@@ -2,25 +2,13 @@ $(document).ready(function() {
     $('.item').click(function(){
         var index=$(this).data('option');
         //console.log(index)
-        var tabs=$('.container_item');
-        var btns=$('.item');
+
         
-        for(var i=1;i<tabs.length+1;i++)
-        {
-            if(i==index)
-            {
-                //display this tab
-                //console.log(tabs[i-1]);
-                btns[i-1].className='item active';
-                tabs[i-1].className='container_item active';
-            }
-            else
-            {
-                //hide other tabs
-                btns[i-1].className='item';
-                tabs[i-1].className='container_item';
-            }
-        }
+        $('.item').removeClass('active');
+        $(this).addClass('active');
+
+        $('.container_item').hide();
+        $('.container_item[data-item="'+index+'"]').show();
     });
 
 
